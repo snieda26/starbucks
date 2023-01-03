78b85bb7e92351341f4940cec319a780ca592502
US <us@MacBook-Pro-US.local>
2023-01-03 07:45:10 +0200

    first commit

---
+++ app/components/layout/Test.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function testlayout() {
+    return (
+        <h1>test layout</h1>
+    )
+}
+
+export default testlayout
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
+import TestLayout from '@/layout/Test'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
